75c522c7bdac42bb4b69d9921505279d1f1bef5e
Bel Borba <isaborba.cs@gmail.com>
2025-09-01 23:47:51 -0300

    chore: initial setup with Vite, TailWind, Alias and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-amber-200">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
