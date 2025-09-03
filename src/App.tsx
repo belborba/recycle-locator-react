7417f30f7c83e992a15a70247d1bea5de877f1b2
Bel Borba <isaborba.cs@gmail.com>
2025-09-02 21:58:44 -0300

    feat: add socialbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./index.css";
+import { SocialMedia } from "./Layout/Components/SocialMedia";
 
 function App() {
   return (
     <>
+      <SocialMedia />
       <p className="text-amber-200">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/Layout/Components/SocialMedia/index.tsx
@@ -0,0 +1,9 @@
+import IgIcon from "@/assets/icons/ig-icon.svg?react";
+
+export const SocialMedia = () => {
+  return (
+    <div className="w-full h-auto flex justify-end p-6 bg-amber-700">
+      <IgIcon />
+    </div>
+  );
+};
