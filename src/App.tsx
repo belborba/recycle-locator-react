9e83c1e83881902857f734fcddfa1e807d23e587
Bel Borba <isaborba.cs@gmail.com>
2025-09-03 11:26:56 -0300

    style: add background image

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { SocialMedia } from "./Layout/Components/SocialMedia";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col justify-between items-center">
+    <div className="flex min-h-screen flex-col justify-between items-center bg-neutral-100 bg-[url('@/assets/images/bg.svg')] bg-no-repeat bg-cover bg-center">
       <SocialMedia />
       <p>Click on the Vite and React logos to learn more</p>
       <Footer />
